7df02e18aa202d134c85089be5839115494f356a
Melona-Png <madisont.woodward@gmail.com>
2023-02-01 21:35:14 -0500

    added index.html and started skeleton

---
+++ src/generateHtml.js
@@ -0,0 +1,20 @@
+htmlContent += `
+      <div class="Employee">
+        <h2>${employee.name}</h2>
+        <p>ID: ${employee.id}</p>
+        <p>Email: ${employee.email}</p>
+        <p>Office#: ${employee.office}</p>
+      </div>
+    `;
+    
+
+let html = `
+    <html>
+      <head>
+        <title>Team</title>
+      </head>
+      <body>
+        ${htmlContent}
+      </body>
+    </html>
+  `;
