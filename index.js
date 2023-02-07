bb3c6b67a3f2e4c3086cefff3f7455cafface555
Melona-Png <madisont.woodward@gmail.com>
2023-02-06 19:36:48 -0500

    made changes to generateHTML and added index/style, pushing to save

--- index.js
+++ index.js
@@ -129,9 +129,3 @@ function createIntern(){
 function createTeam(){
   console.log(team)
 }
-// todo: Ecercise OOP: creating class and extending class.
-  // Exercise test: test class and pass all.
-  // Exercise Inquirer library: prompt user from command line.
-  // Using user inputs and instantiate instances from different classes.
-  // Using generated objects to pass value to a new HTML
-  // Created a new HTML index file using all the input data
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,20 +1,39 @@
-htmlContent += `
-      <div class="Employee">
-        <h2>${employee.name}</h2>
-        <p>ID: ${employee.id}</p>
-        <p>Email: ${employee.email}</p>
-        <p>Office#: ${employee.office}</p>
-      </div>
-    `;
-    
+function generateHTML() {
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="./styles.css">
+      <title>My Team</title>
+  </head>
+  
+  <body>
+      <header>
+          <h1>MY TEAM</h1>
+      </header><main class = "card-container">`;
 
-let html = `
-    <html>
-      <head>
-        <title>Team</title>
-      </head>
-      <body>
-        ${htmlContent}
-      </body>
-    </html>
-  `;
+  for (const team of employeeInfo) {
+      html += `
+      <div class="card">
+        <div class="name">
+          <p class="card-name">${team.name}</p>
+          <p class="card-title">${team.getRole()}</p>
+        </div>
+        <div class="card-body">
+          <div class="card-items">
+            <p class="card-1">ID: ${team.id}</p>
+            <p class="card-2">EMAIL: <a href="mailto:${team.email}">${team.email}</a> </p>
+            <p class="card-3">${team instanceof Manager ? 'Office Number: ' + team.office
+            : employee instanceof Engineer? `GitHub: <a href='https://github.com/${employee.github}'>${employee.github}</a>` : 
+            'School: ' + employee.school
+          }</p>
+          </div>
+        </div>
+      </div>`;
+  }
+  html += `</main> </body> </html>`
+  fs.writeFileSync("./public/index.html", html);
+}
